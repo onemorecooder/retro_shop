591e46d62d64b350512430742265f13921950a55
Alejandro <ajosetorrespiedra97@gmail.com>
2023-03-24 23:29:33 +0100

    adding carousel and links

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -8,16 +8,7 @@ function AboutUs() {
         <h1 className="text-5xl font-medium text-center">¿Quienes somos?</h1>
         <h1 className="text-2xl font-light text-center mt-1">
           ¡Hola! Somos una empresa que se dedica a la venta de componentes y
-          recambios para consolas retro. Ofrecemos productos de alta calidad
-          para modificar y actualizar tus consolas clásicas, como pantallas
-          retroiluminadas, botoneras, carcasas y baterías recargables.
-        </h1>
-
-        <h1 className="text-2xl font-light text-center mt-4">
-          Además, compartimos nuestra pasión por los juegos retro en nuestras
-          redes sociales y blog con noticias, tutoriales y guías. ¡Contáctanos
-          para obtener los mejores componentes y recambios para tu consola
-          clásica!
+          recambios para consolas retro.
         </h1>
       </div>
       <Carousel />
